89774b92deaaa26935df7ac0b4de82b5fef5f24f
Javiera Saavedra <javiera.saavedra@uc.cl>
2025-06-09 00:03:55 -0400

    fix(modal): add border to Modal component for improved visibility

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -7,7 +7,7 @@ type ModalProps = {
 const Modal = ({ children }: ModalProps) => {
     return (
         <div className="absolute inset-0 bg-white/60 flex justify-center items-start z-50 pt-[12vh]">
-            <div className="bg-white shadow-lg rounded-lg min-w-md max-w-lg min-h-sm max-h-lg flex flex-col gap-4">
+            <div className="bg-white shadow-lg border border-border-color/50 rounded-lg min-w-md max-w-lg min-h-sm max-h-lg flex flex-col gap-4">
                 {children}
             </div>
         </div>
